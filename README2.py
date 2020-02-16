6d1eefb092836801f37f1b4e1b742fba7f779f4c
kikihjt <506936183@qq.com>
2020-02-16 17:02:15 +0800

    读取excel指定列输出到txt

---
+++ README2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import xlrd
+
+# 打开excel文件
+rbook = xlrd.open_workbook('read.xlsx')
+rsheet = rbook.sheets()[0]  # 取第一个工作簿
+#ncols=rsheet.ncols
+
+t= open("1.txt","w")
+
+for row in rsheet.get_rows():
+	write_value=row[2]
+	result=str(write_value)
+	t.write(result+'\n')
+
+t.close()
